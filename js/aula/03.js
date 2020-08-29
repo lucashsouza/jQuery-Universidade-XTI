372669c60ddcc327afe4a8b488e5f9f9c1907523
Lucas Souza <lhenriquesouza00@gmail.com>
2020-08-29 13:18:37 -0300

    Universidade XTI - jQuery - 04 - Seletores Simples

---
+++ js/aula/03.js
@@ -0,0 +1,9 @@
+// Click no botão altera cor do elemento para Azul
+$(document).ready(function() {
+    //$("*").css("color", "#0000FF");
+    $("#titulo").css("color", "green");
+    $("a").css("color", "red");
+    $(".outro").css("color", "black");
+    $("h1, a").css("background-color", "yellow");
+
+});
